38555a57cee966e5ae02e3f1c04055cffab9192e
Olga <olga.kohanova@gmail.com>
2020-03-28 14:37:47 +0300

    first commit

---
+++ data.js
@@ -0,0 +1,19 @@
+module.exports = function () {
+  return {
+    products: [{
+        id: 1,
+        name: "Chess Board",
+        category: "Chess",
+        description: "A fun game",
+        price: 75
+      },
+      {
+        id: 2,
+        name: "Chess Board 2",
+        category: "Chess",
+        description: "A fun game 2",
+        price: 45
+      }
+    ]
+  }
+}
