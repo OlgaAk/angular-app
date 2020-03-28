1c370e95e7a95b83b714303c3c90a65b74f93a38
Olga <olga.kohanova@gmail.com>
2020-03-28 15:11:24 +0300

    json server working

--- authMiddleware.js
+++ authMiddleware.js
@@ -20,5 +20,21 @@ module.exports = function (req, res, next) {
     }
     res.end()
     return
+  } else if (
+    (req.url.startsWith("/products") || req.url.startsWith("/categories")) && req.method != "GET" ||
+    req.url.startsWith("/orders") && req.method != "POST") {
+    let token = req.headers["authorization"]
+    if (token != null && token.startsWith("Bearer<")) {
+      token = token.substring(7, token.length - 1)
+      try {
+        jwt.verify(token, APP_SECRET)
+        next()
+        return
+      } catch (err) {}
+    }
+    res.statusCode = 401
+    res.end()
+    return
   }
+  next()
 }
