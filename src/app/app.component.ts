c886602d5a4c4f09bae47b34f1052f548ed5619e
Olga <olga.kohanova@gmail.com>
2020-04-02 22:23:21 +0300

    added categories selection + pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template: `<div class="bg-success p-2 text-center text-white">This is Sportsstore</div>`
+  template: "<store></store>"
   // templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
 })
--- src/app/model/static.datasource.ts
+++ src/app/model/static.datasource.ts
@@ -6,7 +6,12 @@ import { Observable, from } from "rxjs"
 export class StaticDataSource {
     private products: Product[] = [
         new Product(1, "Product 1", "Category 1", "Product 1 (Category 1)", 100),
-        new Product(2, "Product 2", "Category 2", "Product 2 (Category 2)", 200)
+        new Product(2, "Product 2", "Category 2", "Product 2 (Category 2)", 200),
+        new Product(3, "Product 3", "Category 1", "Product 3 (Category 1)", 50),
+        new Product(4, "Product 4", "Category 1", "Product 4 (Category 1)", 10),
+        new Product(5, "Product 5", "Category 2", "Product 5 (Category 2)", 200),
+        new Product(6, "Product 6", "Category 2", "Product 6 (Category 2)", 100),
+        new Product(7, "Product 7", "Category 2", "Product 7 (Category 2)", 350)
     ]
     getProducts():
         Observable<Product[]> {
