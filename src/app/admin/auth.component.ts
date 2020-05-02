b939020ae025cb2022831ec6569dcf00b6932a37
Olga <olga.kohanova@gmail.com>
2020-05-02 22:50:15 +0300

    added authentication logic with auth service and auth guard

--- src/app/admin/auth.component.ts
+++ src/app/admin/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core"
 import { NgForm } from '@angular/forms'
 import { Router } from "@angular/router"
+import { AuthService } from "../model/auth.service"
 
 @Component({
     templateUrl: "auth.component.html"
@@ -9,11 +10,16 @@ export class AuthComponent {
     public username: string;
     public password: string;
     public errorMessage: string;
-    constructor(private router: Router) { }
+    constructor(private router: Router, private auth: AuthService) { }
     authenticate(form: NgForm) {
         if (form.valid) {
-            //todo
-            this.router.navigateByUrl("/admin/main")
+            this.auth.authenticate(this.username, this.password).subscribe(response => {
+                if (response) {
+                    this.router.navigateByUrl("/admin/main")
+                }
+                this.errorMessage = "Authentification Failed"
+            })
+
         } else {
             this.errorMessage = "Form Data Invalid"
         }
