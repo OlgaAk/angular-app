13709d3380c559efc1f1eda684c968d27e3da090
Olga <olga.kohanova@gmail.com>
2020-06-01 22:43:38 +0300

    admin area extended

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core"
+import { Router } from "@angular/router"
+import { AuthService } from "../model/auth.service"
 @Component({
     templateUrl: "admin.component.html"
 })
---
+++ src/app/admin/productTable.component.ts
@@ -0,0 +1,6 @@
+import { Component } from "@angular/core"
+
+@Component({
+    template: `<div class="bg-info p-2 text-white"><h3>ProductTable placeholder</h3></div>`
+})
+export class ProductTableComponent { }
